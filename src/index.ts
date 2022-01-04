1b485c32af2af619a4995dbed2afcf7d47a65130
Hung <miaooo.sl.ys.lyn.hyeon.lb@gmail.com>
2022-01-04 15:01:46 +0700

    test bot

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+dotenv.config();
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
